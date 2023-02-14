f805c07e70453f9b6a138dbe91834d90b1f99915
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 22:12:30 +0500

    New Guest list

---
+++ src/ex14.ts
@@ -0,0 +1,11 @@
+/*
+    Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+    so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+*/
+const guests: string[] = ["Salman", "Mehboob", "Abid", "Kashif"]
+console.log(`${guests[0]} i would like to invite you to a dinner party.`)
+console.log(`${guests[1]} i would like to invite you to a dinner party.`)
+console.log(`${guests[2]} i would like to invite you to a dinner party.`)
+console.log(`${guests[3]} i would like to invite you to a dinner party.`)
+
+export { }
