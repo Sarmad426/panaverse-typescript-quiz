402484b0cef6c155f0b0d89d913dc4fc19902723
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 21:18:41 +0500

    Name stripping exercise

---
+++ src/ex5.ts
@@ -0,0 +1,10 @@
+/*
+    Stripping Names: Store a person’s name, and include some whitespace characters at the beginning
+    and end of the name. Make sure you use each character combination, "\t" and "\n", at least once.
+    Print the name once, so the whitespace around the name is displayed. Then print the name 
+    after striping the white spaces.
+*/
+const person_name:string = "\t Sarmad \t Rafique";
+console.log(person_name);
+
+export{}
