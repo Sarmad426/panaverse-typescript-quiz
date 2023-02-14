c4565e4d45161cff4779b48be192a6634e03a480
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-15 01:43:50 +0500

    Array sorting and reversing

---
+++ src/ex17.ts
@@ -0,0 +1,5 @@
+let favorite_places: string[] = ["Madina", "Makkah", "Palestine", "India", "Singapore"]
+let unsorted_list = favorite_places;
+console.log(favorite_places.sort())
+console.log(unsorted_list)
+console.log(favorite_places.reverse())
