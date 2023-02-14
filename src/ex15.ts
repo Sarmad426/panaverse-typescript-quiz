77e0f0fb0954956d0bf801c790adbec93e06c805
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 22:49:28 +0500

    Added more guests

---
+++ src/ex15.ts
@@ -0,0 +1,21 @@
+const guests: string[] = ["Salman", "Mehboob", "Abid", "Tahmoor"]
+console.log(`${guests[0]} i would like to invite you to a dinner party.`)
+console.log(`${guests[1]} i would like to invite you to a dinner party.`)
+console.log(`${guests[2]} i would like to invite you to a dinner party.`)
+console.log(`${guests[3]} is not coming to the party of some reason.`)
+// Updated the Guest
+guests[3] = "Fakhir"
+console.log(`${guests[0]} i would like to invite you to a dinner party.`)
+console.log(`${guests[1]} i would like to invite you to a dinner party.`)
+console.log(`${guests[2]} i would like to invite you to a dinner party.`)
+console.log(`${guests[3]} i would like to invite you to a dinner party.`)
+console.log(`\n I just Found a bigger Dinner Table.`)
+guests.unshift("Akmal")
+guests.push("Naveed");
+console.log(`${guests[0]} i would like to invite you to a dinner party.`)
+console.log(`${guests[1]} i would like to invite you to a dinner party.`)
+console.log(`${guests[2]} i would like to invite you to a dinner party.`)
+console.log(`${guests[3]} i would like to invite you to a dinner party.`)
+console.log(`${guests[4]} i would like to invite you to a dinner party.`)
+console.log(`${guests[5]} i would like to invite you to a dinner party.`)
+export { }
