355f8a465fa8337074073ad130e9afaa2791483e
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 22:04:59 +0500

    favorite things

---
+++ src/ex12.ts
@@ -0,0 +1,10 @@
+/*
+    Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, 
+    and make a list that stores several examples. Use your list to print a series of statements 
+    about these items, such as “I would like to own a Honda motorcycle.”
+*/
+const vision: string[] = ["Audi", "Honda Civic", "MG Car", "PSL Team"];
+console.log(`I Would like to own a ${vision[0]}.`);
+console.log(`I Would like to own a ${vision[1]}.`);
+console.log(`I Would like to own a ${vision[2]}.`);
+console.log(`I Would like to own a ${vision[3]}.`);
