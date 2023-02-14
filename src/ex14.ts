b1f7328a5fb2c37cfb83a1b1a8e36f33e5918887
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 22:19:26 +0500

    Added the logic of quiz that was previously missing

--- src/ex14.ts
+++ src/ex14.ts
@@ -2,10 +2,15 @@
     Changing Guest List: You just heard that one of your guests can’t make the dinner, 
     so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
 */
-const guests: string[] = ["Salman", "Mehboob", "Abid", "Kashif"]
+const guests: string[] = ["Salman", "Mehboob", "Abid", "Tahmoor"]
+console.log(`${guests[0]} i would like to invite you to a dinner party.`)
+console.log(`${guests[1]} i would like to invite you to a dinner party.`)
+console.log(`${guests[2]} i would like to invite you to a dinner party.`)
+console.log(`${guests[3]} is not coming to the party of some reason.`)
+// Updated the Guest
+guests[3] = "Fakhir"
 console.log(`${guests[0]} i would like to invite you to a dinner party.`)
 console.log(`${guests[1]} i would like to invite you to a dinner party.`)
 console.log(`${guests[2]} i would like to invite you to a dinner party.`)
 console.log(`${guests[3]} i would like to invite you to a dinner party.`)
-
 export { }
