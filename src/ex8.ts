006c9f039a2c019179fa545dbe46793ec0ba56c4
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 21:30:40 +0500

    Favorite number exercise

---
+++ src/ex8.ts
@@ -0,0 +1,7 @@
+/*
+    Favorite Number: Store your favorite number in a variable. Then, using that variable,
+     create a message that reveals your favorite number. Print that message.
+*/
+
+const favorite_number: number = 7;
+console.log(`Favorite Number: ${favorite_number}`)
