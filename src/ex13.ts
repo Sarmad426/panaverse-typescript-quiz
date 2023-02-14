2e02cd96dcb976f0a61f06ed2af21e2c9008dff5
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 22:09:13 +0500

    Guests invitation

---
+++ src/ex13.ts
@@ -0,0 +1,10 @@
+/*
+    Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+    Make a list that includes at least three people you’d like to invite to dinner. 
+    Then use your list to print a message to each person, inviting them to dinner.
+*/
+const guests: string[] = ["Salman", "Mehboob", "Abid", "Tahmoor"]
+console.log(`${guests[0]} i would like to invite you to a dinner party.`)
+console.log(`${guests[1]} i would like to invite you to a dinner party.`)
+console.log(`${guests[2]} i would like to invite you to a dinner party.`)
+console.log(`${guests[3]} i would like to invite you to a dinner party.`)
