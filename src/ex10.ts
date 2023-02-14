071acb24c585b273b9f8749c20b05ae07a521516
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 21:42:13 +0500

    Friends name exercise

---
+++ src/ex10.ts
@@ -0,0 +1,11 @@
+/*
+    Names: Store the names of a few of your friends in array called names. 
+    Print each person’s name by accessing each element in the list, one at a time.
+*/
+const names: string[] = ["Salman", "Kashif", "Mehboob", "Tahmoor", "Abid",];
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[4])
+export { }
