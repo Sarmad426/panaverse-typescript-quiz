da47fc65355413a083a2051583c4b48fd16f393f
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 20:48:19 +0500

    Implemented exercise 2

---
+++ src/ex2.ts
@@ -0,0 +1,8 @@
+/*
+    Name Cases: Store a person’s name in a variable, and then
+     print that person’s name in lowercase, uppercase, and title case.
+*/
+const personName:string = "Babar Azam";
+
+console.log(personName.toUpperCase())
+console.log(personName.toLowerCase())
