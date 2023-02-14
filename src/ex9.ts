13022ab1cbca480dd5d7c187e7d00862aad638ec
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 21:37:32 +0500

    Adding comments exercise

---
+++ src/ex9.ts
@@ -0,0 +1,12 @@
+/*
+        Adding Comments: Choose two of the programs you’ve written, and add at least one
+        comment to each. If you don’t have anything specific to write because your programs 
+        are too simple at this point, just add your name and the current date at the top of
+        each program file. Then write one sentence  describing what the program does.
+*/
+
+// This program displays my favorite number in the console
+const favorite_number: number = 7;
+console.log(`Favorite Number: ${favorite_number}`)
+
+export { }
