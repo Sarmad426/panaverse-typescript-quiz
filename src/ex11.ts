f043338a6771e28c79f900298f706a8fdbe16d5d
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-14 21:56:46 +0500

    Message friend name

---
+++ src/ex11.ts
@@ -0,0 +1,13 @@
+/*
+    Greetings: Start with the array you used in Exercise 11, but instead of just
+    printing each person’s name,   print a message to them.  The text of each message 
+    should be the same, but each message should be personalized with the person’s name.
+*/
+const friend_names: string[] = ["Salman", "Kashif", "Mehboob", "Tahmoor", "Abid",];
+console.log(`${friend_names[0]}, You are my good Friend: `)
+console.log(`${friend_names[1]}, You are my good Friend: `)
+console.log(`${friend_names[2]}, You are my good Friend: `)
+console.log(`${friend_names[3]}, You are my good Friend: `)
+console.log(`${friend_names[4]}, You are my good Friend: `)
+
+export { }
