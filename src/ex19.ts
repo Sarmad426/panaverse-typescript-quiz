8a67b24614ef9d11ebfa55d5f6ebeb7f711d39f9
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-15 23:45:17 +0500

    Array Exercise

---
+++ src/ex19.ts
@@ -0,0 +1,6 @@
+let countryNames: string[] = ["Pakistan", "India", "China", "Russia", "Afghanistan", "Australia"];
+countryNames.map((country: string): void => {
+    console.log(country);
+})
+
+export { }
