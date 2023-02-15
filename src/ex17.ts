3625b6ee8d3f6228894f56f367a19c65947bab55
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-15 23:31:41 +0500

    arrays sorting and reverse sorting exercise

--- src/ex17.ts
+++ src/ex17.ts
@@ -1,5 +1,12 @@
 let favorite_places: string[] = ["Madina", "Makkah", "Palestine", "India", "Singapore"]
 let unsorted_list = favorite_places;
-console.log(favorite_places.sort())
+let sorted_list = favorite_places;
+console.log(favorite_places);
+console.log(sorted_list.sort())
 console.log(unsorted_list)
-console.log(favorite_places.reverse())
+console.log(sorted_list.reverse())
+console.log(favorite_places)
+console.log(sorted_list.sort())
+console.log(favorite_places);
+console.log(sorted_list.reverse())
+console.log(unsorted_list)
