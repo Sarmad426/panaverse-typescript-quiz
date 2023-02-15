b8774920ed9e71b7694b69f3984d897de8f47a8f
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-16 00:05:45 +0500

    Updated the Exercise with more true false prompts

--- src/ex22.ts
+++ src/ex22.ts
@@ -1,5 +1,49 @@
+// 1
 let car = 'Toyota';
 
 console.log("Is car == 'Toyota'? I predict True.")
 
 console.log(car == 'Toyota')
+
+// 2
+let game = "GTA"
+console.log("Is Game == 'GTA' ? I predict True.")
+console.log(game == 'GTA')
+
+// 3
+let player = "khiladi"
+console.log("Is player == 'khiladi' ? I predict True.")
+console.log(player == 'khiladi')
+// 4
+let website = "amazon"
+console.log("Is Website == 'airbnb' ? I predict False.")
+console.log(website == 'airbnb')
+// 5 
+let blanket = "GM"
+console.log("Is Blanket == 'GM' ? I predict True.")
+console.log(blanket == 'GM')
+// 6
+let register = "Karimia"
+console.log("Is Register == 'Karimia' ? I predict False.")
+console.log(register == 'Fancy')
+
+// 7
+let laptop = "Dell"
+console.log("Is Laptop == 'Dell' ? I predict False.")
+console.log(laptop == 'Dell')
+
+//8
+let computer = "Hp"
+console.log("Is computer == 'Lenovo' ? I predict True.")
+console.log(computer == 'Lenovo')
+
+//9
+let bag = "Hp"
+console.log("Is Bag == 'Dell' ? I predict False.")
+console.log(bag == 'Dell')
+
+
+// 10
+let drink = "Coca Cola"
+console.log("Is Drink == 'Pepsi' ? I predict False.")
+console.log(game == 'Pepsi')
