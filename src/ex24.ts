9598de0a6505c39b95e275ec1635c0f23d8e894b
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-17 22:41:29 +0500

    alien color 2 exercise

---
+++ src/ex24.ts
@@ -0,0 +1,15 @@
+let alien_color = "red";
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points for shooting the alien.");
+} else {
+    console.log("The player just earned 10 points.");
+}
+// second version
+let alien_color2 = "green";
+if (alien_color2 === 'green') {
+    console.log("The player just earned 5 points for shooting the alien.");
+} else {
+    console.log("The player just earned 10 points.");
+
+}
+export { }
