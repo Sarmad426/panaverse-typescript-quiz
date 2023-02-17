e842c40a4688bfe3472b75a5b1d700a219d6e756
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-17 22:46:05 +0500

    alien color 3

---
+++ src/ex25.ts
@@ -0,0 +1,9 @@
+let alien_color = "yellow"
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points for shooting the alien.");
+} else if (alien_color === "yellow") {
+    console.log("The player just earned 10 points.");
+} else if (alien_color === "red") {
+    console.log("The player just earned 15 points.");
+}
+export { }
