5ec9ec018c447e211cd5fc0689792637fa7b801b
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-18 20:32:30 +0500

    Added more fruits to array and also added more if conditions

--- src/ex27.ts
+++ src/ex27.ts
@@ -1,4 +1,4 @@
-let favorite_fruits: string[] = ["Banana", "Apple", "Mango"];
+let favorite_fruits: string[] = ["Banana", "Apple", "Mango", "Strawberries", "Orange"];
 favorite_fruits.map((fruit: string): void => {
     if (fruit.toLowerCase() === 'apple') {
         console.log(`You Really Like ${fruit}`);
@@ -7,6 +7,12 @@ favorite_fruits.map((fruit: string): void => {
         console.log(`You Really Like ${fruit}`);
     }
     if (fruit.toLowerCase() === 'mango') {
+        console.log(`You Really Like ${fruit}`);
+    }
+    if (fruit.toLowerCase() === 'strawberries') {
+        console.log(`You Really Like ${fruit}`);
+    }
+    if (fruit.toLowerCase() === 'orange') {
         console.log(`You Really Like ${fruit}`);
     }
 })
