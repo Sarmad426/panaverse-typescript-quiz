44bbc08d2f4dfa599b91ebd302f094a5a0e2fc78
Sarmad rafique <sarmadrafique040@gmail.com>
2023-02-19 00:04:18 +0500

    Removed the useless comments

--- src/ex29.ts
+++ src/ex29.ts
@@ -1,14 +1,6 @@
 let current_users: string[] = ["Sarmad", "Fakhir", "Bakhtiar", "Akmal", "Nawaz"];
 let new_users: string[] = ["Sarmad", "Mosh", "Admin", "Akmal", "Martin"];
 for (let user in new_users) {
-    // for (let counter: number = 0; counter <= current_users.length; counter++) {
-    //     if (new_users[user] == current_users[counter]) {
-    //         console.log(`${new_users[user]} is not available.`);
-    //     }
-    //     else if (counter == 5 && new_users[user] != current_users[counter]) {
-    //         console.log(`${new_users[user]} available.`);
-    //     }
-    // }
     if (current_users.includes(new_users[user])) {
         console.log(`${new_users[user]} is not available.`);
     } else {
