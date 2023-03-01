fe1bd780b62507852fc29b19c0998b79623cc982
Sarmad rafique <sarmadrafique040@gmail.com>
2023-03-01 14:53:26 +0500

    Added a check for both uppercase and lowercase.

--- src/ex29.ts
+++ src/ex29.ts
@@ -1,7 +1,7 @@
 let current_users: string[] = ["Sarmad", "Fakhir", "Bakhtiar", "Akmal", "Nawaz"];
 let new_users: string[] = ["Sarmad", "Mosh", "Admin", "Akmal", "Martin"];
 for (let user in new_users) {
-    if (current_users.includes(new_users[user])) {
+    if (current_users.includes(new_users[user]) || current_users.includes(new_users[user].toLowerCase())) {
         console.log(`${new_users[user]} is not available.`);
     } else {
         console.log(`${new_users[user]} available.`);
