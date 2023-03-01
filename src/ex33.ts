790b64236dff20b02e2c37f89f7fa12aee12dfb2
Sarmad rafique <sarmadrafique040@gmail.com>
2023-03-01 15:27:43 +0500

    Shirt Size and Shirt TExt

---
+++ src/ex33.ts
@@ -0,0 +1,5 @@
+const makeShirt = (size: number, shirt_text: string): void => {
+    console.log(`Shirt Size: ${size}`)
+    console.log(`Shirt Text: ${shirt_text}`);
+}
+makeShirt(42, "Always Be Progressive: 💛");
