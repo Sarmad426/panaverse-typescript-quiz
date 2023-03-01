4880ec257b9dbd8c7cebe722fd2f6c4cbf8191d9
Sarmad rafique <sarmadrafique040@gmail.com>
2023-03-01 15:38:27 +0500

    Shirt Sizes, Large ,small and any size

---
+++ src/ex34.ts
@@ -0,0 +1,12 @@
+const makeShirt = (size: number, shirt_text: string): void => {
+    if (size < 40) {
+        console.log(`Large Shirt Size: ${size}`)
+    }
+    else {
+        console.log(`Large Shirt Size: ${size}`)
+    }
+    console.log(`Shirt Text: ${shirt_text}`);
+    console.log(`Any Size Shirt: ${size}`);
+}
+makeShirt(42, "Always Be Progressive: 💛");
+export { }
