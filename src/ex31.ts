a45bbae8169c7c5b4ec3b1b116c2d410a98592da
Sarmad rafique <sarmadrafique040@gmail.com>
2023-03-01 15:07:25 +0500

    Favorite pizza array

---
+++ src/ex31.ts
@@ -0,0 +1,10 @@
+const favorite_pizzas: string[] = ["Pepproni", "Cheez", "Bar-B-Q"]
+for (let pizza in favorite_pizzas) {
+    console.log(favorite_pizzas[pizza])
+}
+for (let pizza in favorite_pizzas) {
+    console.log(`I like ${favorite_pizzas[pizza]} Pizza.`)
+}
+console.log("\nI like Pizza Very Much.");
+console.log("Pizza is one of the most delicious dishes i have ever eaten");
+console.log("I really Like Pizza and its Variety of Tastes.");
