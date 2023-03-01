aed75657cc6d4ddbbf79a963f3854cf86aa3053d
Sarmad rafique <sarmadrafique040@gmail.com>
2023-03-01 15:01:52 +0500

    Ordinal Numbers

---
+++ src/ex30.ts
@@ -0,0 +1,33 @@
+const counting: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let count in counting) {
+    if (counting[count] === 1) {
+        console.log(`${counting[count]}st`);
+    }
+    else if (counting[count] === 2) {
+        console.log(`${counting[count]}nd`);
+    }
+    else if (counting[count] === 3) {
+        console.log(`${counting[count]}rd`);
+    }
+    else if (counting[count] === 4) {
+        console.log(`${counting[count]}th`);
+    }
+    else if (counting[count] === 5) {
+        console.log(`${counting[count]}th`);
+    }
+    else if (counting[count] === 6) {
+        console.log(`${counting[count]}th`);
+    }
+    else if (counting[count] === 7) {
+        console.log(`${counting[count]}th`);
+    }
+    else if (counting[count] === 8) {
+        console.log(`${counting[count]}th`);
+    }
+    else if (counting[count] === 9) {
+        console.log(`${counting[count]}th`);
+    }
+    else if (counting[count] === 10) {
+        console.log(`${counting[count]}th`);
+    }
+}
