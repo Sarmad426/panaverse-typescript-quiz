3dcf1b11390a355e4f34f732e9a375267ac766c4
Sarmad rafique <sarmadrafique040@gmail.com>
2023-03-05 15:02:35 +0500

    City Country function Exercise

---
+++ src/ex36.ts
@@ -0,0 +1,6 @@
+const cityCountry = (city: string, country: string): void => {
+    console.log(`${city} , ${country}`)
+}
+cityCountry("Lahore", "Pakistan");
+cityCountry("Delhi", "India");
+cityCountry("London", "England");
