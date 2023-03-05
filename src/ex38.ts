988d6cbcb8c59e6ec2ca7b4e6dd38088d8cae820
Sarmad rafique <sarmadrafique040@gmail.com>
2023-03-05 15:45:16 +0500

    Magicians Array

---
+++ src/ex38.ts
@@ -0,0 +1,4 @@
+const magicians: string[] = ["Harry", "Henry", "Bray Wyatt", "Brock Lesnar", "Shelton Benjamen", "John Cena", "Roman Reigns"];
+for (let magician in magicians) {
+    console.log(`${magicians[magician]} is a Magician`);
+} 
